c3647239de61128fa8dffded7cbbb7800d66ed4c
Andri 33 <saputroandri1991@gmail.com>
2026-09-24 14:41:55 +0700

    fix(websocket): create initial notifications table

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Notification(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField(blank=True)
+    type = models.CharField(max_length=20, default="info")
+    data = models.JSONField(default=dict, blank=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
